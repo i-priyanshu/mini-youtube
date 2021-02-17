ba8a58844d0bf08607c3319b570b16ae99a47522
Priyanshu Kumar <priyanshupboy0786@gmail.com>
2021-02-18 00:04:54 +0530

    API setup

--- youtube-react/src/components/App.js
+++ youtube-react/src/components/App.js
@@ -1,8 +1,32 @@
 import React from "react";
+import SearchBar from "./searchBar";
+import Youtube from "../APIs/youtube";
+
+const KEY = 'AIzaSyBEjU_vd4nncN3o8CnqseCXjiRk6Rj_oCs';
 
 class App extends React.Component {
+  state = {videos : []};
+
+  onTermSubmit = async term => {
+   const response = await Youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        key:KEY
+      }
+    });
+
+    this.setState({videos : response.data.items });
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
+      </div>
+    );
   }
 }
 
--- youtube-react/src/index.js
+++ youtube-react/src/index.js
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-//this is a comment added to test the git commit chnages
-
 ReactDOM.render(<App />, document.querySelector("#root"));
